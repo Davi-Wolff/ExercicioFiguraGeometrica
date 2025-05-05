edac315e0a9fb44c60339512e1d51c358c6e6fa0
Davi Wolff <>
2025-05-05 11:00:27 -0300

    Início do projeto.

---
+++ src/main/java/model/Circulo.java
@@ -0,0 +1,33 @@
+package model;
+
+/**
+ *
+ * @author Davi Wolff
+ */
+public class Circulo extends FiguraGeometrica {
+
+    private double raio;
+
+    public Circulo() {
+        this("", 0);
+    }
+
+    public Circulo(String cor, double raio) {
+        super(cor);
+        this.raio = raio;
+    }
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public double getArea() {
+        return (Math.PI * getRaio() * getRaio());
+    }
+
+}
